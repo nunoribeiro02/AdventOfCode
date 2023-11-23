dd643c3fa2c21a944b48dd35d6b6a1fb11f9cb10
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2023-11-23 22:27:46 +0000

    feat: added day 6 part 1 solution

---
+++ 2022/day06/day06_part1.py
@@ -0,0 +1,26 @@
+import sys
+
+def allDifferent(signal: str) -> bool:
+    different = True
+    for i in range(len(signal)):
+        for j in range(i+1, len(signal)):
+            if (signal[i] == signal[j]):
+                different = False
+                break
+        if not different:
+            break
+    
+    return different
+
+if __name__ == "__main__":
+    input = open(sys.argv[1]).read()
+    indexUpper = 4
+    indexLower = 0
+    signal = input[indexLower : indexUpper]
+
+    while (not allDifferent(signal)):
+        indexUpper += 1
+        indexLower += 1
+        signal = input[indexLower : indexUpper]
+
+    print(f"{indexUpper} characters need processed before the first start-of-packet marker is detected")
